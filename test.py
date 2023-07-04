93b93e747365840331ac57ccc666a5e017a8bc84
leonelsandoval <sandovalleonel16@gmail.com>
2023-07-04 09:59:40 -0500

    test

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ desviacion_estandar = df['Columna'].std()
 limite_inferior = media - 2 * desviacion_estandar
 limite_superior = media + 2 * desviacion_estandar
 
+
 datos_dentro_del_rango = df[(df['Columna'] >= limite_inferior) & (df['Columna'] <= limite_superior)]
 
 print(datos_dentro_del_rango)
